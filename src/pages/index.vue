ddfd9b74d5ec02f7b08e73edab87e6d41788fd10
henribru <6639509+henribru@users.noreply.github.com>
2021-09-30 17:39:58 +0200

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { Popover, PopoverPanel, PopoverButton } from '@headlessui/vue'
+
+defineProps({
+  message: String,
+})
+</script>
+
+<template>
+  <div>
+    <Popover>
+      <PopoverButton>Click me</PopoverButton>
+      <PopoverPanel>Foo</PopoverPanel>
+    </Popover>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
